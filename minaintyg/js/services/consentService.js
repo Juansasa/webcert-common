a9e1104519e207c9ceec2a9af9a2005d06ee302f
Quang vu <nickiewooster@gmail.com>
2014-10-26 15:02:34 +0100

    added required services

---
+++ minaintyg/js/services/consentService.js
@@ -0,0 +1,30 @@
+angular.module('common').factory('minaintyg.consentService',
+    function($http, $log) {
+        'use strict';
+
+        function _giveConsent(callback) {
+            $http.post('/api/certificates/consent/give', {}).success(function(data) {
+                callback(data);
+            }).error(function(data, status) {
+                $log.error('error ' + status);
+                // give calling code a chance to handle error
+                callback(null);
+            });
+        }
+
+        function _revokeConsent(callback) {
+            $http.post('/api/certificates/consent/revoke', {}).success(function(data) {
+                callback(data);
+            }).error(function(data, status) {
+                $log.error('error ' + status);
+                // give calling code a chance to handle error
+                callback(null);
+            });
+        }
+
+        // Return public API for service
+        return {
+            giveConsent: _giveConsent,
+            revokeConsent: _revokeConsent
+        };
+    });
